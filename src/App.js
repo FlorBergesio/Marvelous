8d5dbb6041a46cd9c1dec26cb219ab7c36231bd9
Florencia - Ubuntu 20.04 <ing.florencia.bergesio@gmail.com>
2021-08-23 11:22:41 -0300

    Added Callback hook to manage change of data source by user

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import './App.css';
 import sampleData from './sampleData.js';
 import Button from './components/button';
@@ -12,30 +12,30 @@ function App() {
   const [dataRetrieved, setDataRetrieved] = useState([]);
   const [footerExtraInfo, setfooterExtraInfo] = useState('2021');
 
-  useEffect( () => {
-    async function fetchMarvelAPI() {
-      let response = await fetch(
-        `${process.env.REACT_APP_MARVEL_API_URL}/characters?nameStartsWith=White&ts=${process.env.REACT_APP_MARVEL_API_TS}&apikey=${process.env.REACT_APP_MARVEL_API_KEY}&hash=${process.env.REACT_APP_MARVEL_API_HASH}`
-      );
-      const dataFromAPI = await response.json();
-      setDataRetrieved(dataFromAPI.data.results);
-      setfooterExtraInfo(dataFromAPI.attributionText)
-    }
+  const handleClickChangeDataSource = useCallback( () => {
+    setwithSampleData( ( currentValue ) => !currentValue );
+  }, [] );
 
+  useEffect( () => {
     if ( withSampleData ) {
       // With sample data
       setfooterExtraInfo(sampleData.attributionText)
       setDataRetrieved(sampleData.data.results);
     } else {
       // With API calls
+      async function fetchMarvelAPI() {
+        let response = await fetch(
+          `${process.env.REACT_APP_MARVEL_API_URL}/characters?nameStartsWith=White&ts=${process.env.REACT_APP_MARVEL_API_TS}&apikey=${process.env.REACT_APP_MARVEL_API_KEY}&hash=${process.env.REACT_APP_MARVEL_API_HASH}`
+        );
+        const dataFromAPI = await response.json();
+        setDataRetrieved(dataFromAPI.data.results);
+        setfooterExtraInfo(dataFromAPI.attributionText)
+      }
+
       fetchMarvelAPI();
     }
 
   }, [withSampleData] );
-
-  const handleClickChangeDataSource = () => {
-    setwithSampleData(!withSampleData);
-  };
 
   let content;
 
